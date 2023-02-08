a4efab27413d416a5513953a486695683ed2bf80
Mauricio Hernandez <mauricio.hernandez@devsu.com>
2023-02-08 12:02:26 -0600

    Typewritter library implemented, header styled

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,12 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Hero from '../components/Hero'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='bg-[#242424] text-white h-screen'>
       <Head>
         <title>Mauro's Portfolio</title>
       </Head>
@@ -15,6 +16,10 @@ const Home: NextPage = () => {
       <Header/>
 
       {/* Hero */}
+
+      <section id='hero'>
+        <Hero/>
+      </section>
 
       {/* About */}
 
